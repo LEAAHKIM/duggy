b8fd316fff851a77445f0ae96bdc1a994350b8dc
LEAAHKIM <lk2935@barnard.edu>
2024-07-28 16:12:27 +0900

    Initial Commit

---
+++ duggy/duggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  duggyApp.swift
+//  duggy
+//
+//  Created by Leah Kim on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct duggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
